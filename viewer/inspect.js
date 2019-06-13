0cda3a8925b72dabc9bb198ea29281de8ea6a377
Fontany--Legall Brandon <brandon.fontany--legall@etu.unice.fr>
2019-06-13 19:18:28 +0200

    graph reset

--- viewer/inspect.js
+++ viewer/inspect.js
@@ -19,6 +19,11 @@ class Graph{
         Graph.tips.push(tips);
     }
 
+    static reset(){
+        var svg = d3.select("svg > g");
+        svg.selectAll("*").remove();
+    }
+
     static draw(){
 
         // Data for this example
